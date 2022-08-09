471e6f2c3fc980ffbf0a2772553732d18d7f5b47
jacobamisano <jpamisano@gmail.com>
2022-08-09 08:41:33 -0500

    Added functionality to some TriggerEffects and minor player methods

--- CardEffect.js
+++ CardEffect.js
@@ -5,15 +5,44 @@ class CardEffect {
     private requireLast;
     private player;
 
-    constructor(effectValue, effectType, requireLast = false, player) {
+    constructor(effectValue:number, effectType:EffectType, requireLast = false, player:Player) {
         this.effectValue = effectValue;
         this.effectType = effectType;
         this.requireLast = requireLast;
         this.player = player;
     }
 
-    triggerEffect() {
-        
+    triggerEffect() : Boolean{
+        switch (this.effectType) {
+            case EffectType.AddLevel: {
+                this.player.addLevel(this.effectValue);
+                break;
+            }
+
+            case EffectType.LoseGear: {
+                if(this.effectValue != GearSlots.OneHand){
+                    this.player.gear[this.effectValue].pop();
+                } else if ((this.player.gear[4] !=  null) && (this.player.gear[5] == null)) {
+                    this.player.gear.splice(4, 1);
+                } else if ((this.player.gear[5] !=  null) && (this.player.gear[4] == null)){
+                    this.player.gear.splice(5, 1);
+                } else {
+                    //TODO: Prompt user with a selection between player.gear at index 4 and index 5
+                    console.log("Player has two one-handed items equipped");
+                }
+                break;
+            }
+
+            case EffectType.Die: {
+                this.player.setLevel(0);
+                this.player.isAlive(false);
+                break;
+            }
+
+            case EffectType.Escape: {
+                //TODO: End the current battle
+            }
+        }
     }
 
 }
@@ -21,8 +50,7 @@ class CardEffect {
 const EffectType = {
     AddLevel : "addLevel",
     DrawTreasure : "drawTreasure",
-    AddBonus : "addBonus",
     LoseGear : "loseGear",
     Die : "die",
-    AddGold : "addGold"
+    Escape : "escape"
 }
